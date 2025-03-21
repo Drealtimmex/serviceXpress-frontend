28a2293177b2d3a66cbc548fecc8166588eb648f
drealtimmex <agboolaafeez413@gmail.com>
2025-03-21 13:09:15 -0700

    Updated  new  user routes

--- servicexpress/src/app/layout.tsx
+++ servicexpress/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import "./globals.css";
-import type { Metadata } from "next";
+ import type { Metadata } from "next";
 import { Inter,Poppins } from "next/font/google";
 import RedirectToWaitlist from "./redirectWaitlist"; // Import the redirection component
 
